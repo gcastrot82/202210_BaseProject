b4649bbd8ae60bdf7ebb1337f8b251c27dba12b4
Gerardo Castro Tamayo <gcastrot82@hotmail.com>
2022-04-30 07:24:41 -0500

    Update01: Se agrega modulo para cafés

---
+++ src/app/cafe/cafe.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CafeListComponent } from './cafe-list/cafe-list.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports:[CafeListComponent],
+  declarations: [CafeListComponent]
+})
+export class CafeModule { }
